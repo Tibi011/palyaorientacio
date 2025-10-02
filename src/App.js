9c859ad6644c5a7263a5e9cc3f0e563b6cdba671
Tibi011 <megsztil@gmail.com>
2025-10-02 14:20:30 +0200

    3

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
     <div className="App">
       <h1>Pályaorientáció</h1>
       <p>Ha pályaválasztás előtt állsz, érdemes
-tisztázni, hogy milyen képességekkel rendelkezel. A képességek erősen befolyásolják, hogy
-pályádon, egy foglalkozás gyakorlásánál mennyire leszel sikeres. Az alábbi kérdőív ebben jelenthet
-segítséget: kitöltésével tájékozódhatsz a képességeidről, melyek jelentik az erősségeidet és melyek
-azok, amelyekben gyengébb vagy.
+          tisztázni, hogy milyen képességekkel rendelkezel. A képességek erősen befolyásolják, hogy
+            pályádon, egy foglalkozás gyakorlásánál mennyire leszel sikeres. Az alábbi kérdőív ebben jelenthet
+            segítséget: kitöltésével tájékozódhatsz a képességeidről, melyek jelentik az erősségeidet és melyek
+            azok, amelyekben gyengébb vagy.
 </p>
     </div>
   );
